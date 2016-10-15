4f6449157b25ef8c6fd2d3ca54d2b487d671c73b
atlas0904 <u920243@gmail.com>
2016-10-15 14:54:19 +0800

    Fix divided by zero problem

--- hw1/calculator.rb
+++ hw1/calculator.rb
@@ -6,6 +6,14 @@ def isNumber(v)
   v =~ /\A[-+]?[0-9]*\.?[0-9]+\Z/
 end
 
+def isDenominatorZero(num2, op)
+  r = (op == "/" && num2 == 0)
+  if r
+    puts "Denominator is zero. Please enter non-zero value"
+    return r
+  end
+end
+
 begin
   puts "Please enter number1:"
 end while (!isNumber(num1 = gets.chomp.upcase))
@@ -16,7 +24,7 @@ end while !["+", "-", "*", "/"].include?(op = gets.chomp.upcase)
 
 begin
   puts "Please enter number2:"
-end while (!isNumber(num2 = gets.chomp.upcase))
+end while (!isNumber(num2 = gets.chomp.upcase) || isDenominatorZero(num2.to_i, op))
 
 result = nil;
 case op
@@ -27,5 +35,3 @@ when "/" then result = num1.to_i / num2.to_i
 end
 
 puts "The operation result= #{result}"
-
-
